7f905fb0566a71a0b314aadda00ad6f6a4a8b185
tkpguy <116381625+pvrklrd@users.noreply.github.com>
2023-08-18 20:43:54 +0530

    tables_wip

---
+++ PG$LCLHST.session.sql
@@ -0,0 +1,12 @@
+create table modeling.ivl_dim_machine_tbl(
+    "country_cd" char(3) not null,
+    "plant_cd" varchar(250) not null,
+    "mold_cd" varchar(250) not null,
+    "mold_wt_grm" decimal(15,2) not null,
+    "mold_cavities" int not null,
+    "mold_cycle_time" decimal(15,2) not null,
+    "mold_name" as concat(mold_cd, '/', mold_wt_grm, '/', mold_cavities,'/' ,mold_cycle_time) PERSISTED,
+    "mmold_descr" as concat(country_cd,'/' ,plant_cd,'/', mold_cd, '/', mold_wt_grm,'/', mold_cavities, '/', mold_cycle_time) PERSISTED,
+    "mold_sku" as concat(mold_cd, '/', mold_wt_grm, '/', mold_cavities) PERSISTED,
+    
+) 
